6658717df9dfe63edf464cb3f1e5f5f73083b734
cloudsoftwareoff <chahid.ahmadi2@gmail.com>
2024-06-12 16:21:08 +0100

    starter

---
+++ lib/screens/auth/main_auth.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:scoltender/screens/auth/login.dart';
+import 'package:scoltender/screens/auth/register.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => LoginOrRegisterPageState();
+}
+
+class LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  //initially show login Page
+
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(
+        onTap: togglePages,
+      );
+    } else {
+      return RegisterPage(
+        onTap: togglePages,
+      );
+    }
+  }
+}
